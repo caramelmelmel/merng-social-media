af5ff51fb9d57fb65639e1f9090e2195bb1ae420
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-05-13 06:41:08 +0800

    changed mutation and return, added register

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+//import user mongoose model
+const {SECRET_KEY} = require('../../config')
+const User = require('../../models/User')
+
+module.exports = {
+    Mutation:{
+        async register(_,
+            {
+                registerInput:{username, email, password, confirmPassword}
+            },
+            context,info){
+            //TODO validate user data 
+            //TODO make sure user doesn't already exist
+            
+            //hash password and create auth token
+            password = await bcrypt.hash(password,12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save()
+            const userInfo = {
+                id:res.id,
+                email: res.email,
+                username: res.username
+            }
+            const token = jwt.sign(userInfo,SECRET_KEY,{expiresIn: '1h'});
+
+            return{
+                ...res._doc,
+                id:res._id,
+                token
+            }
+        }
+    }
+}
