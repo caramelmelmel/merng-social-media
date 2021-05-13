d5cba35a49fefbeb60e313a398a8ec214df986f1
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-05-13 23:46:05 +0800

    Registration half done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.production.min'
+import {Button, Form} from 'semantic-ui-react'
 
 function Register() {
+
+    //initiate the initial state
+    const [values, setValues] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const onChange = (event)=>{
+        setValues({...values, [event.target.name]: event.target.value})
+    }
+
+    const onSubmit = (event)=>{
+        event.preventDefault();
+        
+    }
     return(
         <div>
-            <h1>Register page</h1>
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input
+                label="Username"
+                placeholder="Username.."
+                name="username"
+                value={values.username}
+                onChange={onChange}
+                />
+                <Form.Input
+                label="Email"
+                placeholder="Email.."
+                name="email"
+                value={values.email}
+                onChange={onChange}
+                />
+                <Form.Input
+                label="Password"
+                placeholder="Password.."
+                name="password"
+                value={values.username}
+                onChange={onChange}
+                />
+                <Form.Input
+                label=" Confirm Password"
+                placeholder=" Confirm Password.."
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={onChange}
+                />
+                <Button type="submit" primary>
+                    Register
+                </Button>
+            </Form>
         </div>
     )
 }
