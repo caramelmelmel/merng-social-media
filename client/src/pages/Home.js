eefa6a5352f8c161633706723bb0b53969c89f95
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-05-14 08:17:03 +0800

    changed destructed data to be defined as object fixed error

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,12 +8,12 @@ import PostCard from '../components/PostCard';
 function Home() {
   const {
     loading,
-    data: { getPosts: posts }
+    data: { getPosts: posts } = {}
   } = useQuery(FETCH_POSTS_QUERY);
 
   return (
     <Grid columns={3}>
-      <Grid.Row className='page-title'>
+      <Grid.Row className="page-title">
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
