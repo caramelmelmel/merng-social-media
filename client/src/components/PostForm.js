ccf56ade84c6b7df22371a26e490c9bc389b6b8c
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-05-21 20:05:49 +0800

    like button add

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -19,7 +19,7 @@ function PostForm(){
                 query: FETCH_POSTS_QUERY
             })
             data.getPosts = [result.data.createPost, ...data.getPosts]
-            proxy.writeQuery({data: FETCH_POSTS_QUERY, data})
+            proxy.writeQuery({query: FETCH_POSTS_QUERY, data})
             values.body=''
         }
     })
