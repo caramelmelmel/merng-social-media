8ba4df6d284bca6f5cfd06fdcdfe7382b1a3f55e
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-05-23 23:27:32 +0800

    code cleanup for small use case here

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 import gql from 'graphql-tag'
 import {useMutation} from '@apollo/react-hooks'
-import {Button, Confirm, Icon} from 'semantic-ui-react'
+import {Button, Confirm, Icon, Popup} from 'semantic-ui-react'
 
 import {FETCH_POSTS_QUERY} from '../util/graphql'
+import MyPopup from "../util/MyPopup";
+
 function DeleteButton({postId, commentId, callback}){
 
     //confirm is a boolean
@@ -32,14 +34,19 @@ function DeleteButton({postId, commentId, callback}){
     });
     return(
         <>
-      <Button
-        as="div"
-        color="red"
-        floated="right"
-        onClick={() => setConfirmOpen(true)}
-      >
-        <Icon name="trash" style={{ margin: 0 }} />
-      </Button>
+        <MyPopup
+          content={commentId ? 'Delete comment' : 'Delete post'}
+          trigger={
+            <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => setConfirmOpen(true)}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+          }
+          />
       <Confirm
         open={confirmOpen}
         //click on yes or no to delete
